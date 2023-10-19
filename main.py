7ada8614924d34dde99c8e2f4dc13f52118ec6e6
desktop <haydenguyn@gmail.com>
2023-10-18 20:14:07 -0700

    Added sliders and labels to the UI

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import maya.cmds as cmds
 import maya.OpenMaya as om
-from PySide2.QtWidgets import QMainWindow, QApplication, QVBoxLayout
+from PySide2.QtWidgets import QMainWindow, QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QSlider
+from PySide2.QtCore import Qt
 
 class CameraShake(QMainWindow):
     def __init__(self):
         super().__init__()
-        
-        self.setWindowTitle("Camera Shake")
-
         self.create_ui()
 
     def create_ui(self):
-        layout = QVBoxLayout()
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
 
-        self.setLayout(layout)
+        main_layout = QHBoxLayout(central_widget)
+
+        lb_layout = QVBoxLayout()
+        lb_frequency = QLabel("Frequency: ")
+        lb_amplitude = QLabel("Amplitude: ")
+        lb_layout.addWidget(lb_frequency)
+        lb_layout.addWidget(lb_amplitude)
+
+        slider_layout = QVBoxLayout()
+        frequency_slider = QSlider(Qt.Horizontal)
+        amplitude_slider = QSlider(Qt.Horizontal)
+        frequency_slider.setRange(0, 10)
+        amplitude_slider.setRange(0, 10)
+        slider_layout.addWidget(frequency_slider)
+        slider_layout.addWidget(amplitude_slider)
+
+        main_layout.addLayout(lb_layout)
+        main_layout.addLayout(slider_layout)
 
 if __name__ == '__main__':
     app = QApplication.instance()
