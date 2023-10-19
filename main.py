1d1ee9069d5eef97bea9dbb8108bb96a6f1e6819
desktop <haydenguyn@gmail.com>
2023-10-19 13:11:56 -0700

    Changed the get_selected_cameras logic to get the transform instead of the Shape

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import maya.cmds as cmds
 import maya.OpenMaya as om
+import random
+
 from PySide2.QtWidgets import QMainWindow, QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QSlider
 from PySide2.QtCore import Qt
 
@@ -12,6 +14,7 @@ class CameraShake(QMainWindow):
         self.amplitude_slider.valueChanged.connect(self.camera_amplitude_update)
 
         self.selected_cameras = self.get_selected_cameras()
+        self.num_frames = self.get_max_frame_number()
 
     # Creates the UI 
     def setupUI(self):
@@ -37,25 +40,34 @@ class CameraShake(QMainWindow):
         main_layout.addLayout(lb_layout)
         main_layout.addLayout(slider_layout)
 
-    # Returns a list of the selected cameraShapes in the scene
+    # Returns a list of the selected camera transforms in the scene
     def get_selected_cameras(self):
-        default_cameras = ('perspShape', 'topShape', 'frontShape', 'sideShape')
-        
-        selected_objects = cmds.ls(selection=True, dag=True)
-        all_cameras = cmds.ls(type="camera")
+        all_cameras = cmds.ls(type="camera", long=True)
 
-        selected_cameras = [cam for cam in all_cameras if cam in selected_objects and cam not in default_cameras]
+        all_camera_transforms = [cmds.listRelatives(cam, parent=True)[0] for cam in all_cameras]
+
+        selected_objects = cmds.ls(selection=True)
+
+        selected_cameras = [cam for cam in selected_objects if cam in all_camera_transforms]
         
         return selected_cameras
 
     # Gets the number of frames in the scene and returns its value as an integer
     def get_max_frame_number(self):
-        self.frame_nums = int(cmds.playbackOptions(q=True, maxTime=True))
+        num_frames = int(cmds.playbackOptions(q=True, maxTime=True))
 
-        return self.frame_nums
+        return num_frames
     
     def camera_frequency_update(self):
-        print("placeholder")
+        value = self.frequency_slider.value()
+
+        for frame in range(self.num_frames):
+            random_translation = [random.uniform(-value, value), random.uniform(-value, value), random.uniform(-value, value)]
+
+            for camera in self.selected_cameras:
+                cmds.setKeyframe(camera, attribute="translateX", t=frame, value=random_translation[0])
+                cmds.setKeyframe(camera, attribute="translateY", t=frame, value=random_translation[1])
+                cmds.setKeyframe(camera, attribute="translateZ", t=frame, value=random_translation[2])
 
     def camera_amplitude_update(self):
         print("placeholder")
