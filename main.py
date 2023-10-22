84c4e6855969c963e8e2fea25ac12110c3cf209f
desktop <haydenguyn@gmail.com>
2023-10-22 15:32:15 -0700

    Added comments throughout

--- main.py
+++ main.py
@@ -5,82 +5,106 @@ import random
 from PySide2.QtWidgets import QMainWindow, QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QSlider, QPushButton
 from PySide2.QtCore import Qt
 
+# Define the CameraShake class for the application window
 class CameraShake(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        # Call the setupUI class to create the user interface
         self.setupUI()
 
+        # Initialize slider values
         self.value_x = self.frequency_x_slider.value()
         self.value_y = self.frequency_y_slider.value()
         self.value_z = self.frequency_z_slider.value()
 
+        # Connect slider value change signals to the freq_slider_values_updated function
         self.frequency_x_slider.valueChanged.connect(self.freq_slider_values_updated)
         self.frequency_y_slider.valueChanged.connect(self.freq_slider_values_updated)
         self.frequency_z_slider.valueChanged.connect(self.freq_slider_values_updated)
 
+        # Connect button click signals to the apply/reset_values functions
         self.apply_button.clicked.connect(self.apply_values)
         self.reset_button.clicked.connect(self.reset_values)
 
+        # Get a list of selected cameras 
         self.selected_cameras = self.get_selected_cameras()
+
+        # Get the number of frames in the scene
         self.num_frames = self.get_max_frame_number()
 
-    # Creates the UI 
+    # Define the UI layout and widgets
     def setupUI(self):
+
+        # Create central widget
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
 
+        # Create main layout
         main_layout = QVBoxLayout(central_widget)
         label_slider_layout = QHBoxLayout()
 
+        # Create label layout and labels
         lb_layout = QVBoxLayout()
-
         lb_frequency_x = QLabel("Frequency X: ")
         lb_frequency_y = QLabel("Frequency Y: ")
         lb_frequency_z = QLabel("Frequency Z: ")
         lb_amplitude = QLabel("Amplitude: ")
 
+        # Add labels to the label layout
         lb_layout.addWidget(lb_frequency_x)
         lb_layout.addWidget(lb_frequency_y)
         lb_layout.addWidget(lb_frequency_z)
         lb_layout.addWidget(lb_amplitude)
 
+        # Create slider layout and horizontal sliders
         slider_layout = QVBoxLayout()
-
         self.frequency_x_slider = QSlider(Qt.Horizontal)
         self.frequency_y_slider = QSlider(Qt.Horizontal)
         self.frequency_z_slider = QSlider(Qt.Horizontal)
         self.amplitude_slider = QSlider(Qt.Horizontal)
 
+        # Set the slider ranges
         self.frequency_x_slider.setRange(0, 10)
         self.frequency_y_slider.setRange(0, 10)
         self.frequency_z_slider.setRange(0, 10)
         self.amplitude_slider.setRange(1, 10)
 
+        # Add the sliders to the slider layout
         slider_layout.addWidget(self.frequency_x_slider)
         slider_layout.addWidget(self.frequency_y_slider)
         slider_layout.addWidget(self.frequency_z_slider)
         slider_layout.addWidget(self.amplitude_slider)
 
+        # Create a button layout and buttons
         button_layout = QHBoxLayout()
-        
         self.apply_button = QPushButton("Apply")
         self.reset_button = QPushButton("Reset")
+
+        # Add the buttons to the button layout
         button_layout.addWidget(self.apply_button)
         button_layout.addWidget(self.reset_button)
 
+        # Add the label and slider layouts to a label/slider layout
         label_slider_layout.addLayout(lb_layout)
         label_slider_layout.addLayout(slider_layout)
+
+        # Add the label_slider and button layouts to the main layout
         main_layout.addLayout(label_slider_layout)
         main_layout.addLayout(button_layout)
 
     # Returns a list of the selected camera transforms in the scene
     def get_selected_cameras(self):
+        # List all cameras in the scene
         all_cameras = cmds.ls(type="camera", long=True)
 
+        # Get the parent transform nodes for every camera
         all_camera_transforms = [cmds.listRelatives(cam, parent=True)[0] for cam in all_cameras]
 
+        # Get the currently selected objects in the scene
         selected_objects = cmds.ls(selection=True)
 
+        # Filter selected objects to only include camera transforms
         selected_cameras = [cam for cam in selected_objects if cam in all_camera_transforms]
         
         return selected_cameras
@@ -91,11 +115,13 @@ class CameraShake(QMainWindow):
 
         return num_frames
     
+    # Divide the freq slider values by 500 whenever slider value is changed
     def freq_slider_values_updated(self):
         self.value_x = self.frequency_x_slider.value() / 500
         self.value_y = self.frequency_y_slider.value() / 500
         self.value_z = self.frequency_z_slider.value() / 500
 
+    # Generate random translation coordinates based on sliders
     def random_translate_coordinate(self):
         random_x = random.uniform(-self.value_x, self.value_x) * self.amplitude_slider.value()
         random_y = random.uniform(-self.value_y, self.value_y) * self.amplitude_slider.value()
@@ -103,6 +129,8 @@ class CameraShake(QMainWindow):
 
         return random_x, random_y, random_z
     
+
+    # Apply camera shake by setting keyframes
     def apply_values(self):        
         for frame in range(self.num_frames):
             random_x, random_y, random_z = self.random_translate_coordinate()
@@ -113,6 +141,7 @@ class CameraShake(QMainWindow):
                     cmds.setKeyframe(camera, attribute="translateY", t=frame, value=random_y)
                     cmds.setKeyframe(camera, attribute="translateZ", t=frame, value=random_z)
 
+    # Reset slider positions/values and remove set keyframes
     def reset_values(self):
         self.frequency_x_slider.setValue(0)
         self.frequency_y_slider.setValue(0)
